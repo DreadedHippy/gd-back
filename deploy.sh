0ec61ee3c2303676b315acb1253c7cc8f2e75aa8
Aizon <onotaizee@gmail.com>
2023-10-09 00:23:50 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "Deploying backend..."
 set -o allexport
 source .env set
 
-docker build --build-arg postgres_admin_name=$POSTGRES_ADMIN_NAME --build-arg database_url=$DATABASE_URL --build-arg postgres_admin_pass=$POSTGRES_ADMIN_PASS -t $DOCKER_IMAGE_NAME .
+docker build -t $DOCKER_IMAGE_NAME .
 
 if [ $? -eq 0 ]; then
   echo "Image built successfully"
